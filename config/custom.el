ec45e7e1a732cd70e20edf335f72432e8e8ac5d2
Larry Wright <larrywright@gmail.com>
2009-01-26 14:28:32 -0600

    Making some local variables safe

--- config/custom.el
+++ config/custom.el
@@ -3,7 +3,8 @@
   ;; If you edit it by hand, you could mess it up, so be careful.
   ;; Your init file should contain only one such instance.
   ;; If there is more than one, they won't work right.
- '(jabber-account-list (quote (("larrywright@jabber.afni.net" (:password . "hunter") (:port . 5238))))))
+ '(jabber-account-list (quote (("larrywright@jabber.afni.net" (:password . "hunter") (:port . 5238)))))
+ '(safe-local-variable-values (quote ((require-final-newline) (backup-inhibited . t) (ruby-compilation-executable . "ruby") (ruby-compilation-executable . "ruby1.8") (ruby-compilation-executable . "ruby1.9") (ruby-compilation-executable . "rbx") (ruby-compilation-executable . "jruby")))))
 (custom-set-faces
   ;; custom-set-faces was added by Custom.
   ;; If you edit it by hand, you could mess it up, so be careful.
